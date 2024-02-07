5082f1ded5b83d52fe88ec8721955ce42bd5679d
Rajib Sarker <redhat0593@gmail.com>
2024-02-08 01:10:05 +0600

    trigger solved

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,15 +1,24 @@
 const triggers = document.querySelectorAll(".top");
 const linkBoxes = document.querySelectorAll(".links");
 
-triggers.forEach((trigger, index) => {
-    trigger.addEventListener("click", () => {
-        const computedStyle = window.getComputedStyle(linkBoxes[index]);
-        const currentHeight = computedStyle.getPropertyValue("height");
+var screenWidth = window.screen.width;
 
-        if (currentHeight === '0px' || currentHeight === '0') {
-            linkBoxes[index].style.height = 'auto';
-        } else {
-            linkBoxes[index].style.height = '0px';
-        }
+if (screenWidth < 768) {
+    triggers.forEach((trigger, index) => {
+        trigger.addEventListener("click", () => {
+            const computedStyle = window.getComputedStyle(linkBoxes[index]);
+            const currentHeight = computedStyle.getPropertyValue("height");
+
+            if (currentHeight === '0px' || currentHeight === '0') {
+                linkBoxes.forEach(box => {
+                    box.style.height = '0';
+                });
+                linkBoxes[index].style.height = 'auto';
+            } else {
+                linkBoxes[index].style.height = '0px';
+            }
+        });
     });
-});
+}
+
+
